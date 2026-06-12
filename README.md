73a5021fdc2271126e2352aecb131058225d873d
Broski <109301662+BitSiphon@users.noreply.github.com>
2026-06-12 16:15:08 -0400

    scaffold a minimum c++ grpc connection

--- README.md
+++ README.md
@@ -5,3 +5,30 @@ It's kinda planning. more so ramblings of ideas.
 ### HTTP Learning.
 
 The goal is not highly optimized performance (its a lot of work) or intuitive functionality like golang (though it would be great). The goal is to get file listing and file downloading via HTTP 1.1, and then adding SSL via openssl later.
+
+### broski connect
+
+Calling it a RAT (remote access tool) implies it is malicious, so I'm going to stick with "broski connect". Only thing that seperates a bad actor and someone helping you is whether they get permission. *I do not condone malignant hacking*.
+
+Milestones:
+- [ ] Establish an unauthenticated gRPC connection between a client and server
+- [ ]
+
+Dependencies:
+```bash
+sudo pacman -S base-devel cmake grpc protobuf
+```
+
+Building:
+```bash
+mkdir build && cd build
+
+cmake ..
+
+make -j$(nproc)
+```
+
+If clangd is getting annoyed by the generated proto definitions you need to great a symbolic link:
+```bash
+ln -s build/compile_commands.json .
+```
---
+++ broski_connect/src/server.cpp
@@ -0,0 +1,45 @@
+#include "service.grpc.pb.h"
+#include "service.pb.h"
+#include <grpcpp/security/server_credentials.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include <grpcpp/support/status.h>
+#include <string>
+
+using broski_connect::AdminService;
+
+class AdminServiceImpl final : public broski_connect::AdminService::Service {
+  grpc::Status SendCommand(
+      grpc::ServerContext *context,
+      const broski_connect::SendCommandRequest *request,
+      broski_connect::SendCommandResponse *reply) override {
+
+    std::cout << "Received command: " << request->command() << std::endl;
+
+    // Mocking execution logic
+    reply->set_exit_code(0);
+    reply->set_output("Executed successfully: " + request->command());
+
+    return grpc::Status::OK;
+  }
+};
+
+void run_server() {
+  std::string addr{"[::]:5000"};
+  AdminServiceImpl service;
+
+  grpc::ServerBuilder builder;
+
+  builder.AddListeningPort(addr, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << addr << std::endl;
+  server->Wait();
+}
+
+int main(int argc, char *argv[]) {
+  run_server();
+  return 0;
+}
