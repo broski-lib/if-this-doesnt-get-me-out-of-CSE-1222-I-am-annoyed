93e538e1c5a96a7f93bc488cf1f572f4908765d1
Broski <109301662+BitSiphon@users.noreply.github.com>
2026-06-25 11:07:38 -0400

    solve 2828

---
+++ leetcode/2828.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class Solution {
+public:
+  bool isAcronym(vector<string> &words, string s) {
+    if (s.length() != words.size()) {
+      return false;
+    }
+
+    for (int i = 0; i < s.length(); i++) {
+      if (words[i][0] != s[i]) {
+        return false;
+      }
+    };
+
+    return true;
+  }
+};
